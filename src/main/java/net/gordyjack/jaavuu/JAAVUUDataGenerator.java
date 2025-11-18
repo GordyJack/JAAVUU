cdf056034910e9a7c171a597d6380163abe522fc
gordyjack <gordyjack.mail@gmail.com>
2025-11-17 20:41:10 -0600

    Init Commit

---
+++ src/main/java/net/gordyjack/jaavuu/JAAVUUDataGenerator.java
@@ -0,0 +1,11 @@
+package net.gordyjack.jaavuu;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class JAAVUUDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
